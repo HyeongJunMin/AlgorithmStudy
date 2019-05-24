75422701926a57a8e9f6e76b837fb5de69f2d024
HyeongJunMin <alsgudwns007@naver.com>
2019-05-24 21:13:10 +0900

    2019-05-24 문자열을 정수로 바꾸기

---
+++ 190524StringToInteger/src/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+	public int solution(String s) {
+		int answer;
+		int chc = 1;
+		
+		
+		switch(	s.charAt(0)	) {
+			case '+':
+			case '-':
+			default : 
+		}	
+				
+		return Integer.parseInt(s) * chc ;
+	}
+}
