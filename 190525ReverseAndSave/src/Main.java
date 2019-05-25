61f845d7e59c5ef13acfdf748d578b475bb7f710
HyeongJunMin <alsgudwns007@naver.com>
2019-05-25 13:15:06 +0900

    2019-05-25 자연수 뒤집어 배열로 만들기

---
+++ 190525ReverseAndSave/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution s = new Solution();
+		System.out.println(Arrays.toString(s.solution(12345)));		
+		
+	}
+
+}
